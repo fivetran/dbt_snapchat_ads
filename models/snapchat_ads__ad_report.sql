601046685bfad512ab2e06451b92012b9055c8fa
Jeremy Owens <jeremy.owens@allergan.com>
2025-01-21 10:40:04 -0500

    add ad_squad and campaign to ad report

--- models/snapchat_ads__ad_report.sql
+++ models/snapchat_ads__ad_report.sql
@@ -27,7 +27,20 @@ with ad_hourly as (
     from {{ var('ad_history') }}
     where is_most_recent_record = true
 
-), aggregated as (
+), ad_squads as (
+
+    select *
+    from {{ var('ad_squad_history') }}
+    where is_most_recent_record = true
+
+), campaigns as (
+
+    select *
+    from {{ var('campaign_history') }}
+    where is_most_recent_record = true
+
+
+) aggregated as (
 
     select
         ad_hourly.source_relation,
@@ -36,6 +49,10 @@ with ad_hourly as (
         account.ad_account_name,
         ad_hourly.ad_id,
         ads.ad_name,
+        ad_squads.ad_squad_id,
+        ad_squads.ad_squad_name,
+        campaigns.campaign_id,
+        campaigns.campaign_name,
         account.currency,
         sum(ad_hourly.swipes) as swipes,
         sum(ad_hourly.impressions) as impressions,
@@ -57,8 +74,14 @@ with ad_hourly as (
     left join account
         on creatives.ad_account_id = account.ad_account_id
         and creatives.source_relation = account.source_relation
+    left join ad_squads
+        on ads.ad_squad_id = ad_squads.ad_squad_id
+        and ads.source_relation = ad_squads.source_relation
+    left join campaigns
+        on ad_squads.campaign_id = campaigns.campaign_id
+        and ad_squads.source_relation = campaigns.source_relation
     
-    {{ dbt_utils.group_by(7) }}
+    {{ dbt_utils.group_by(11) }}
 
 )
 
